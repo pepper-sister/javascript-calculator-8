8ba29e68990f254afe64c6d3fce3573eefec48ba
pepper-sister <yuyu7123@naver.com>
2025-10-15 03:16:15 +0900

    feat: 문자열 입력과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+    async run() {
+        // async는 비동기 처리 => await을 사용하여 문자열 입력까지 대기
+        const STR = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+        MissionUtils.Console.print(`결과 : ${STR}`);
+    }
 }
 
 export default App;
