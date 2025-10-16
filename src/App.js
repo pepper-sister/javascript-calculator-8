9521a93e72077e2d3f3d50eca4287b8638bf6bad
pepper-sister <yuyu7123@naver.com>
2025-10-16 22:29:38 +0900

    feat: Error 출력

--- src/App.js
+++ src/App.js
@@ -2,34 +2,47 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
     async run() {
-        // async는 비동기 처리 => await을 사용하여 문자열 입력까지 대기
-        const STR = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+        try {
+            // async는 비동기 처리 => await을 사용하여 문자열 입력까지 대기
+            const STR = await MissionUtils.Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
-        // 쉼표와 콜론 두 가지 기본 구분자 사용
-        let NUM = STR.split(/,|:/);
+            // 쉼표와 콜론 두 가지 기본 구분자 사용
+            let NUM = STR.split(/,|:/);
 
-        // 커스텀 구분자 시작(S), 종료(E) 인덱스 찾기
-        // / 또는 \n이 문자열에 포함되지 않으면 -1 반환
-        const S = STR.lastIndexOf('/') + 1;
-        const E = STR.indexOf('\\n'); // 문자 \를 나타내기 위해 \를 한번 더 작성하여 escape
+            // 커스텀 구분자 시작(S), 종료(E) 인덱스 찾기
+            // / 또는 \n이 문자열에 포함되지 않으면 -1 반환
+            const S = STR.lastIndexOf('/') + 1;
+            const E = STR.indexOf('\\n'); // 문자 \를 나타내기 위해 \를 한번 더 작성하여 escape
 
-        // 커스텀 구분자 사용시
-        if (S !== -1 && E !== -1) {
-            const DIV = STR.substring(S, E); // 커스텀 구분자 추출
-            const NEW_STR = STR.substring(E + 2); // 숫자와 구분자만 있는 문자열 생성
+            // 커스텀 구분자 사용시
+            if (S !== -1 && E !== -1) {
+                const DIV = STR.substring(S, E); // 커스텀 구분자 추출
+                const NEW_STR = STR.substring(E + 2); // 숫자와 구분자만 있는 문자열 생성
 
-            // 변수와 함께 정규 표현식을 만들기 위해 RegExp() 객체 생성
-            NUM = NEW_STR.split(new RegExp(`,|:|${DIV}`));
+                // 변수와 함께 정규 표현식을 만들기 위해 RegExp() 객체 생성
+                NUM = NEW_STR.split(new RegExp(`,|:|${DIV}`));
+            }
+
+            // 배열을 돌면서 각 요소를 숫자로 결과에 덧셈
+            let RES = 0;
+            NUM.forEach((el) => {
+                RES += Number(el);
+            });
+
+            if (isNaN(RES)) {
+                // Error1: 결과가 NaN인 경우
+                throw new Error('결과 값이 NaN입니다.');
+            } else if (RES < 0) {
+                // Error2: 결과가 음수인 경우
+                throw new Error('결과 값이 음수입니다.');
+            } else {
+                // 결과 출력
+                MissionUtils.Console.print(`결과 : ${RES}`);
+            }
+        } catch (error) {
+            // Error 메시지 출력
+            MissionUtils.Console.print(`[ERROR] : ${error.message}`);
         }
-
-        // 배열을 돌면서 각 요소를 숫자로 결과에 덧셈
-        let RES = 0;
-        NUM.forEach((el) => {
-            RES += Number(el);
-        });
-
-        // 결과 출력
-        MissionUtils.Console.print(`결과 : ${RES}`);
     }
 }
 
