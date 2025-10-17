e80cdb9d347ea3f9c8061f443cbd682062805df7
pepper-sister <yuyu7123@naver.com>
2025-10-18 00:01:01 +0900

    refactor: 변수명 수정

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App {
 
             // 커스텀 구분자 시작(S), 종료(E) 인덱스 찾기
             // / 또는 \n이 문자열에 포함되지 않으면 -1 반환
-            const S = STR.lastIndexOf('/') + 1;
-            const E = STR.indexOf('\\n'); // 문자 \를 나타내기 위해 \를 한번 더 작성하여 escape
+            const START = STR.lastIndexOf('/') + 1;
+            const END = STR.indexOf('\\n'); // 문자 \를 나타내기 위해 \를 한번 더 작성하여 escape
 
             // 커스텀 구분자 사용시
-            if (S !== -1 && E !== -1) {
-                const DIV = STR.substring(S, E); // 커스텀 구분자 추출
-                const NEW_STR = STR.substring(E + 2); // 숫자와 구분자만 있는 문자열 생성
+            if (START !== -1 && END !== -1) {
+                const DIV = STR.substring(START, END); // 커스텀 구분자 추출
+                const NEW_STR = STR.substring(END + 2); // 숫자와 구분자만 있는 문자열 생성
 
                 // 변수와 함께 정규 표현식을 만들기 위해 RegExp() 객체 생성
                 NUM = NEW_STR.split(new RegExp(`,|:|${DIV}`));
