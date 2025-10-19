65e55a9dc507903dc0b3dc562b94810f09d5d89b
pepper-sister <yuyu7123@naver.com>
2025-10-20 02:16:36 +0900

    refactor: 시작 인덱스 상수로 선언

--- src/App.js
+++ src/App.js
@@ -11,17 +11,18 @@ class App {
 
             // 커스텀 구분자 사용시
             const ERROR_NUM = -1;
+            const START_IDX = 2;
             // //로 시작할 때 (커스텀 구분자 형식)
             if (input_str.startsWith('//')) {
                 // 커스텀 구분자 종료 인덱스(end_idx) 찾기
                 const end_idx = input_str.indexOf('\\n'); // 문자 \를 나타내기 위해 \를 한번 더 작성하여 escape
                 if (end_idx === ERROR_NUM) throw new Error('커스텀 구분자 형식이 잘못되었습니다.');
 
-                let custom_delimiter = input_str.substring(2, end_idx); // 커스텀 구분자 추출
+                let custom_delimiter = input_str.substring(START_IDX, end_idx); // 커스텀 구분자 추출
                 if (custom_delimiter === '') throw new Error('커스텀 구분자를 입력하지 않았습니다.'); // 커스텀 구분자를 입력하지 않은 경우
                 if (custom_delimiter.length >= 2) throw new Error('커스텀 구분자는 하나의 문자만 입력할 수 있습니다.'); // 커스텀 구분자의 길이가 2이상인 경우
 
-                const num_part = input_str.substring(end_idx + 2); // 숫자와 구분자만 있는 문자열 생성
+                const num_part = input_str.substring(end_idx + START_IDX); // 숫자와 구분자만 있는 문자열 생성
 
                 // 커스텀 구분자가 메타문자인 경우, \를 한번 더 작성하여 escape
                 custom_delimiter = custom_delimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
