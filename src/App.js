431a487ac72de7725dcbb5797f989b74cda1486b
pepper-sister <yuyu7123@naver.com>
2025-10-20 00:10:59 +0900

    fix: 시작 인덱스 에러 비교 수정

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App {
 
             // 커스텀 구분자 시작(start_idx), 종료(end_idx) 인덱스 찾기
             // / 또는 \n이 문자열에 포함되지 않으면 -1 반환
-            const start_idx = input_str.lastIndexOf('/') + 1;
+            const start_idx = input_str.lastIndexOf('/');
             const end_idx = input_str.indexOf('\\n'); // 문자 \를 나타내기 위해 \를 한번 더 작성하여 escape
 
             // 커스텀 구분자 사용시
             const ERROR_NUM = -1;
             if (start_idx !== ERROR_NUM && end_idx !== ERROR_NUM) {
-                const custom_delimiter = input_str.substring(start_idx, end_idx); // 커스텀 구분자 추출
+                const custom_delimiter = input_str.substring(start_idx + 1, end_idx); // 커스텀 구분자 추출
                 const num_part = input_str.substring(end_idx + 2); // 숫자와 구분자만 있는 문자열 생성
 
                 // 변수와 함께 정규 표현식을 만들기 위해 RegExp() 객체 생성
